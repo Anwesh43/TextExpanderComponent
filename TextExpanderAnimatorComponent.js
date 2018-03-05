72dd13a494a558b505550180c0215ce9ac357121
Anwesh43 <anweshthecool0@gmail.com>
2018-03-05 23:29:40 +0530

    adding State class

--- TextExpanderAnimatorComponent.js
+++ TextExpanderAnimatorComponent.js
@@ -24,4 +24,34 @@ class TextExpanderAnimatorComponent extends HTMLElement {
         this.render()
     }
 }
+class State {
+    constructor() {
+        this.scales = [0, 0]
+        this.j = 0
+        this.prevScale = 0
+        this.dir = 0
+    }
+    update(stopcb) {
+        this.scales[this.j] += 0.1 * this.dir
+        if(Math.abs(this.scales[this.j] - this.prevScale) > 1) {
+            this.scales[this.j] = this.prevScale + this.dir
+            this.j += this.dir
+            if(this.j == this.scales.length || this.j == -1) {
+                this.j -= this.dir
+                this.dir = -1
+                this.prevScale = this.scales[this.j]
+                if(this.j == 0) {
+                    this.dir = 0
+                    stopcb()
+                }
+            }
+        }
+    }
+    startUpdate(startcb) {
+        if(this.dir == 0) {
+            this.dir = 1
+            startcb()
+        }
+    }
+}
 customElements.define('teac-comp', TextExpanderAnimatorComponent)
